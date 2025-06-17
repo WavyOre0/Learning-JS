08d0918daa9f1609d310113fcdfb12ee7366f0f1
WavyOre0 <bobgotgames@gmail.com>
2025-06-17 13:00:51 -0500

    Uploading JS code for topic 11

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,58 @@
+const todolist = [{
+  name: 'input 1',
+  dueDate:'2022-12-22'
+}, {
+  name:'input 2',
+  dueDate: '2022-12-22'}];
+
+renderTodoList();
+
+function renderTodoList(){
+
+  let todolistHTML = '';
+/* Example of generating the HTML */
+  for (let i = 0; i < todolist.length; i++){
+    const todoObject = todolist[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    /*example of deconstructing line 16 and 17 to be shorter */
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+      todolist.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todolistHTML += html;
+  }
+ 
+  document.querySelector('.js-todo-list')
+    .innerHTML = todolistHTML;
+}
+
+  function addTodo(){
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todolist.push({
+    //name: name,
+    //dueDate: dueDate
+    //shortuct when objects proprty & variable names are the same this is called shorthand property syntax
+    name,
+    dueDate
+  });
+
+
+  inputElement.value ='';
+
+  renderTodoList();
+}
+
+function deleteTodo(){
+
+}
